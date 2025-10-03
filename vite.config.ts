8b1da04845021343cdcb7b4a75c93629e20b6a56
askmarrina <askmarrina@gmail.com>
2025-10-03 13:18:23 +0200

    correction of vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 
 export default defineConfig({
-    base: '/portfolio-marina/',
+    base: '/portfolio-marina-final/',
     plugins: [
         react({
         babel: {
